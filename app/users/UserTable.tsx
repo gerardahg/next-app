3158ccf9f395114f0a74fe94fe7cf8a9cac8dd7e
gerardahg <gerardohernandezgomes@gmail.com>
2025-03-14 14:44:57 +0100

    Ciclar usuarios
    Intento de ciclar entre usuarios en orden ASC y DESC

--- app/users/UserTable.tsx
+++ app/users/UserTable.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { sort } from 'fast-sort'
+import Link from 'next/link';
 
 interface User {
     id: number;
@@ -6,20 +8,37 @@ interface User {
     email: string;
 }
 
-const UserTable = async () => {
+interface Props {
+    sortOrder: string
+}
+
+const UserTable = async ({ sortOrder }: Props) => {
+    console.log(sortOrder)
     const response = await fetch('https://jsonplaceholder.typicode.com/users')
     const users: User[] = await response.json()
+
+    let sorted = users
+    if(sortOrder == "asc"){
+        sorted = sort(users).desc((u: User) => u.name)
+        sortOrder = "desc"
+    }
+    else if(sortOrder = "desc") {
+        sorted = sort(users).desc((u: User) => u.name)
+        sortOrder = "asc"
+    }
 
     return (
         <table className='table'>
         <thead>
             <tr>
-            <th>User</th>
+            <th>
+                <Link href={`./users?sortOrder=${sortOrder}`}>Name</Link>
+            </th>
             <th>Email</th>
             </tr>
         </thead>
         <tbody>
-            {users.map(user => <tr key={user.id}>
+            {sorted.map(user => <tr key={user.id}>
             <td>{user.name}</td>
             <td>{user.email}</td>
             </tr>)}
--- app/users/page.tsx
+++ app/users/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import UserTable from './UserTable'
 
-const UsersPage = async () => {
+//Declaramos una interface Props, con el atributo searchParam que contiene un objeto sortOrder de tipo string, este será el objeto que entra por URL
+interface Props {
+  searchParams: { sortOrder: string }
+}
+
+//Entra por parametro a la función flecha 'sortOrder' que seria la variable del URL, tiene que tener el mismo nombre
+const UsersPage = ({ searchParams: { sortOrder } } : Props) => {
   return (
     <>
       <h1>Users</h1>
-      <UserTable/>
+      {/* Aqui llamamos a nuestro contenedor y le pasamos sortOrder por parametro*/}
+      <UserTable sortOrder = { sortOrder }  />
     </>
   )
 }
