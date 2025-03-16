1e207e6ed59a6622f9af00f085257ea1ad933a63
gerardahg <gerardohernandezgomes@gmail.com>
2025-03-16 13:04:02 +0100

    Prisma
    -Testing with Prisma, creating, migrating data bases, using clients and models
    -modified the API  where a database was simulated. Now using a real database and managing it with Prisma and a CRUD to users
    -Also activated prettier formatting

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -1,42 +1,64 @@
-import { NextRequest, NextResponse } from "next/server";
-import schema from "../schema";
+import { NextRequest, NextResponse } from "next/server"
+import schema from "../schema"
+import { prisma } from "@/prisma/client"
 
+//Esta es la interface que define el ID que entra por parametro de la URL
 interface Props {
-    params: { id: number }
+  params: { id: string }
 }
 
-export function GET(request: NextRequest, { params: { id } }: Props) {
-    if (id > 10)
-        return NextResponse.json({ error: "User not found" }, { status: 404 })
+//En este caso cambiamos la interface porque los parámetros de URL son string
+export async function GET(request: NextRequest, { params }: Props) {
+  //Hacemos un fetch del usuario por medio su ID
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) }, //Hay que hacerle un parseInt() para transformar el string del URL a int
+  })
 
-    return NextResponse.json({ id: 1, name: "Gerardo" })
+  //Comprobamos si encontramos al usuario
+  if (!user)
+    return NextResponse.json({ error: "User not found" }, { status: 404 })
+
+  return NextResponse.json(user)
 }
 
-export async function PUT(request: NextRequest, { params: { id } }: Props) {
-    const body = await request.json()
-    //Parse suelta una exception, safeParse no
-    const validation = schema.safeParse(body)
-    if(!validation.success) //1. verificamos el json con schema
-        return NextResponse.json(validation.error.errors, { status: 400 })
+export async function PUT(request: NextRequest, { params }: Props) {
+  const body = await request.json()
+  //Parse suelta una exception, safeParse no
+  const validation = schema.safeParse(body)
+  if (!validation.success)
+    //verificamos el json con nuestro schema de zod
+    return NextResponse.json(validation.error.errors, { status: 400 })
 
-    if(!body.name) //2. Verificamos por nuestra cuenta
-        return NextResponse.json({ error: "name is required" }, { status: 400 })
+  //Buscamos que el usuario exista
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  })
+  if (!user)
+    return NextResponse.json({ error: "User not found" }, { status: 404 })
 
-    const user = { id: 10, name: "Gerardo"}
+  //aca es lo mismo, no updateamos con el body entero sino con parametros especificos para evitar
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  })
 
-    if(user.id != id)
-        return NextResponse.json({ error: "User not found" }, { status: 404 })
-
-    user.name = body.name
-    return NextResponse.json(user)
-
+  return NextResponse.json(updatedUser)
 }
 
-export function DELETE(request: NextRequest, { params: { id } }: Props) {
-    const user = { id: 10, name: "Gerardo" }
+export async function DELETE(request: NextRequest, { params }: Props) {
+  //revisamos si existe el usuario
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  })
+  if (!user)
+    return NextResponse.json({ error: "User not found" }, { status: 404 })
 
-    if(id != user.id)
-        return NextResponse.json({ error: "User not found"}, { status: 404 })
+  const deletedUser = await prisma.user.delete({
+    where: { id: user.id },
+  })
 
-    return NextResponse.json({ message: `User ${user.name} deleted`})
+  return NextResponse.json(deletedUser)
 }
--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,21 +1,36 @@
 //Si queremos mostrar algo al usuario utilizamos 'page' pero si es algo orientado al backend, como manejar datos http se utiliza route
 //Para manejar peticiones http se deben utilizar "route handlers"
 
-import { NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server"
+import { prisma } from "@/prisma/client"
 
 //No se utiliza el objeto 'request' pero si lo quitamos next va a manejar este archivo en el cache, asi que lo añadimos para que no se almacene en cache
-export function GET(request: NextRequest) {
-    //Aqui hariamos un fetch a la BD pero de momento se va a simular
-    return NextResponse.json([
-        { id: 1, name: "Gerardo" },
-        { id: 2, name: "Ricky" },
-    ])
+export async function GET(request: NextRequest) {
+  //Esta es la función de prisma que hace un SELECT, si abrimos un objeto dentro podemos especificar parámetros. EJ: findMany({ where: { email: "gerardo@gmail.com" } })
+  const users = await prisma.user.findMany()
+
+  return NextResponse.json(users)
 }
 
 export async function POST(request: NextRequest) {
-    const body = await request.json()
-    if(!body.name)
-        return NextResponse.json({ error: "Name is required" }, {status: 400})
+  const body = await request.json()
+  if (!body.name)
+    return NextResponse.json({ error: "Name is required" }, { status: 400 })
 
-    return NextResponse.json({ id: 1, name: body.name }, {status: 201})
+  //Debemos revisar si ya existe el usuario primero con ese correo
+  const userExists = await prisma.user.findUnique({
+    where: { email: body.email },
+  })
+  if (userExists)
+    return NextResponse.json({ error: "Email already exists" }, { status: 400 })
+
+  //Al crear un usuario debemos asignar al atributo data lo que enviamos a nuestra BD. No es buena idea enviar el 'body' entero, porque se pueden manipular los datos
+  const user = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  })
+
+  return NextResponse.json(user, { status: 201 })
 }
